1cc8317904a4b265ae2cf871f22620a7ae625899
Marina Barbosa Araujo <marina.ba@gmail.com>
2024-09-11 10:41:36 -0300

    começo módulo E

---
+++ aula13/ambiente.js
@@ -0,0 +1,18 @@
+var c = 1
+do {
+    console.log(`Passo ${c}`)
+    c++ // c=c+1
+} while (c <= 6) // estrutura com teste lógico no final
+
+var c = 1
+while (c <= 4) {
+    console.log(`Passo ${c}`)
+    c++ // c=c+1
+} // estrutura com teste lógico no início e mais usada.
+
+
+/*
+console.log('Passo 1')
+console.log('Passo 2')
+console.log('Passo 3')
+*/
